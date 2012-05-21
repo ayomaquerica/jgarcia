4e8d7966c0b745249657c80b9d75c11b0a018ece
jgarcia <gsusli@hotmail.com>
2012-05-21 13:04:56 +0200

    Commit para las clases clase1.java y clase2.java

---
+++ jgarcia/src/clase1.java
@@ -0,0 +1,14 @@
+
+public class clase1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Soy la clase 1");
+
+	}
+
+}
---
+++ jgarcia/src/clase2.java
@@ -0,0 +1,14 @@
+
+public class clase2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("soy la clase 2");
+
+	}
+
+}
